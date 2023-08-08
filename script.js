6dfc5a06aff12e8cfbb60108c751a0bb973af436
Kevin Kim <43616290+Kevinkim05@users.noreply.github.com>
2023-08-07 22:49:15 -0400

    included singleRound function to determine whether a player wins or loses

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ function getComputerChoice() {
 }
 
 function singleRound(playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        return "Paper beats rock, you lose!";
+    } else if (playerSelection == computerSelection) {
+        return "Draw";
+    } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+        return "Rock beats scissors, you win!";
+    } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        return "Scissors beats paper, you lose!";
+    } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+        return "Paper beats rock, you win!";
+    } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        return "Rock beats scissors, you win!";
+    } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+        return "Scissors beats paper, you win!";
+    } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        return "Rock beats scissors, you lose!";
+    }
+}
+
+console.log(singleRound("paper", "ROCK"))
